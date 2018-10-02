f12438656f61e3c7ff81f5cb374737cc3e85654e
aljarrell <alan.k.jarrell@gmail.com>
2018-10-02 13:16:55 -0400

    added samples to randomize pizzas

--- mypizza.rb
+++ mypizza.rb
@@ -1,5 +1,17 @@
+def size()
+  size = ["small", "medium", "large", "xlarge"].sample
+end
+
+def topping()
+  topping = ["bacon", "mushrooms", "peppers", "onions"].sample
+end
+
+def cheese()
+  cheese = ["mozzarella", "cheddar", "gouda"].sample
+end
+
 def pizza(size, topping, cheese)
   puts "You ordered a #{size} pizza, with #{topping}, and #{cheese} cheese."
 end
 
-pizza("large", "bacon", "mozzarella")
+pizza(size(), topping, cheese)
